0f6a4d2f037bb0b1fe4299c13302033584c8c139
farooqspecials <farooqspecials@gmail.com>
2017-06-02 20:51:58 +0200

    further work

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
 			<h2>Test Your PHP Knowledge</h2>
 			<p>This is a multiple choice quiz to test your knowledge of PHP</p>
 			<ul>
-				<li><strong>Number of Questions: </strong>5</li>
+				<li><strong>Number of Questions: </strong><?php echo $total; ?></li>
 				<li><strong>Type: </strong>Multiple Choice</li>
-				<li><strong>Estimated Time: </strong>4 Minutes</li>
+				<li><strong>Estimated Time: </strong><?php echo $total * .5; ?></li>
 			</ul>
 			<a href="question.php?n=1" class="start">Start Quiz</a>
 		</div>
